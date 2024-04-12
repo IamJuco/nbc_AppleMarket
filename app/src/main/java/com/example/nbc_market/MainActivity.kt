ed219035a9420486206c65577582a85208abca3f
Juseonghyun <wntjdgus99@naver.com>
2024-04-12 16:21:16 +0900

    feat : 알림 기능 완성

--- app/src/main/java/com/example/nbc_market/MainActivity.kt
+++ app/src/main/java/com/example/nbc_market/MainActivity.kt
@@ -3,12 +3,22 @@ package com.example.nbc_market
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
 import com.example.nbc_market.Util.dummyData
 import com.example.nbc_market.Util.dummyItems
 import com.example.nbc_market.databinding.ActivityMainBinding
@@ -26,10 +36,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-//    private val myNotificationID = 1
-//
-//    private val channelID = "default"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -38,6 +44,11 @@ class MainActivity : AppCompatActivity() {
         binding.rvMain.adapter = PostAdapter(dummyItems)
 
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+
+        binding.ivAlarm.setOnClickListener {
+            notificationChannel()
+        }
+
     }
 
     private fun showBackPressedDialog() {
@@ -56,10 +67,57 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 
-//    private fun createNotificationChannel() {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            val channel = NotificationChannel(channelID, "default channel",
-//                NotificationManager.IMPORTANCE_DEFAULT)
-//        }
-//    }
+    private fun notificationChannel() {
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder: NotificationCompat.Builder
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+
+            if (!NotificationManagerCompat.from(this).areNotificationsEnabled()){
+                // 알람 권한 요청
+                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+                    putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                }
+                startActivity(intent)
+            }
+            // 26 버전 이상 일때
+            val channelId = "default"
+            val channelName = "Default Channel"
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                // 채널 설정
+                description = "채널 생성"
+                setShowBadge(true)
+                val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                // 알람 오디오 설정
+                val audioAttributes = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+                setSound(uri, audioAttributes)
+                enableVibration(true)
+            }
+            // 채널을 NotificationChannel에 등록
+            manager.createNotificationChannel(channel)
+
+            // 채널을 이용한 builder 설정
+            builder = NotificationCompat.Builder(this,channelId)
+        }else {
+            // 26 버전 이하 일때
+            builder = NotificationCompat.Builder(this)
+        }
+        // 알람 정보 설정
+        builder.run {
+            setSmallIcon(R.drawable.btn_radius)
+            setWhen(System.currentTimeMillis())
+            setContentTitle("키워드 알림")
+            setContentText("설정한 키워드에 대한 알림이 도착했습니다!!")
+        }
+        manager.notify(1,builder.build())
+
+    }
+
 }
--- app/src/main/java/com/example/nbc_market/PostModel.kt
+++ app/src/main/java/com/example/nbc_market/PostModel.kt
@@ -1,7 +1,10 @@
 package com.example.nbc_market
 
 import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class PostModel (
     val postThumnail: Uri,
     val postTitle: String,
@@ -9,4 +12,4 @@ data class PostModel (
     val postPrice: Int,
     val postComment: Int,
     val postFavorite: Int
-)
+) : Parcelable
